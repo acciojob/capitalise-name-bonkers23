2cdce191dce5fb9e9449d2945baee5227febb813
Shariq Hashmi <127607558+bonkers23@users.noreply.github.com>
2025-01-05 20:04:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function toUpperCase() {
+            var inputField = document.getElementById("fname");
+            inputField.value = inputField.value.toUpperCase();
+        }
